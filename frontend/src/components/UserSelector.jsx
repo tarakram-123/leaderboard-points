4c4d231a0684a858813418db16276432b7ae6220
tarakram-123 <tarakram75693@gmail.com>
2025-07-23 17:26:24 +0530

    Update UserSelector.jsx

--- frontend/src/components/UserSelector.jsx
+++ frontend/src/components/UserSelector.jsx
@@ -10,7 +10,7 @@ const UserSelector = () => {
   const fetchUsers = async () => {
     try {
       setLoading(true);
-      const res = await axios.get("http://localhost:5000/api/users");
+      const res = await axios.get("https://leaderboard-points-backend.onrender.com/api/users");
       setUsers(res.data);
     } catch (error) {
       console.error("Error fetching users:", error);
